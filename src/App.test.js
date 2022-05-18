d777c31e9914ae28f098047675843e9484045ea0
Digital House Brasil <99281226+PedagogiaDHBrasil@users.noreply.github.com>
2022-05-18 10:56:28 -0300

    importando arquivo tsx

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import {render, screen, waitFor} from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
-import App from "./App";
+import App from "./App.tsx";
 
 const mockDispatch = jest.fn();
 
